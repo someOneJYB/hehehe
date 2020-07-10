a646d62552e95aabb53e044e3f55b7ab6dcb5bee
baiyuejiao <baiyuejiao@guazi.com>
2020-07-10 11:25:41 +0800

    增加新算法查询质数和二分查找LRU算法

--- common/a.js
+++ common/a.js
@@ -2,6 +2,8 @@
 //     foo: 1,
 // };
 exports.a1 = true;
-const b = require('./b.js');
+let b = require('./b.js');
 console.log('in a, b.done = %j', b.done);
 exports.a2 = true;
+console.log('again')
+b = require('./b.js');
--- common/b.js
+++ common/b.js
@@ -1,2 +1,3 @@
 const a = require('./a.js');
 console.log('in b, a.a1 = %j, a.a2 = %j', a.a1, a.a2);
+console.log(a, '1122')
